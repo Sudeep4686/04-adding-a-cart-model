510a4f0418aab445e970d5176d60f53ce5e56f45
Sudeep4686 <sudeephipparge@gmail.com>
2023-05-26 08:17:09 +0530

    addingUserAndProductRelationship

--- models/cart.js
+++ models/cart.js
@@ -38,21 +38,37 @@ module.exports = class Cart {
     });
   }
 
-
-  static deleteProduct (id,productPrice) {
-    fs.readFile(p,(err, fileContent)=>{
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
       if (err) {
         return;
       }
-      const updatedCart = {...fileContent};
-      const product = updatedCart.products.find(prod=> prod.id===id);
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+          return;
+      }
       const productQty = product.qty;
-      updatedCart.products= updatedCart.products.filter(prod=> prod.id!==id);
-      updatedCart.totalPrice = updatedCart.totalPrice-productPrice*productQty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
 
-      fs.writeFile(p, JSON.stringify(updatedCart),err=>{
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
     });
-  };
-}
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
+};
